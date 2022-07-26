79c7fc9bfd53ea5ed2509791f1ca1b7c5ba6e318
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-07-26 16:45:13 +0600

    adding navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logo from "../../Image/logo/FieldX.png"
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>this is nav bar</h2>
+        <div className='navbar'>
+            <div className='container'>
+                <div className="logo-holder">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="button-holder">
+                    <NavLink className="login-btn" to="/login">Login</NavLink>
+                    <NavLink className="registration-btn" to="/registration">Registration</NavLink>
+                </div>
+            </div>
         </div>
     );
 };
