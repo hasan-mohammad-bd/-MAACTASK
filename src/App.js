2ef2a806ba03929c5185d9731f306be0e07072db
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-07-26 12:46:36 +0600

    adding route & adding image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
+import PageNotFound from './components/pageNotFound/PageNotFound';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Login from './components/login/Login';
+import Registration from './components/registration/Registration';
+
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar></Navbar> 
+      <Routes>
+      <Route path='/' element={<Home></Home>}></Route>
+        <Route path='*' element={<PageNotFound></PageNotFound>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/registration' element={<Registration></Registration>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
