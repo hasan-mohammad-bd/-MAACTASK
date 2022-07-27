cbd26cc30cbacb8d88840dc4e4f317b44f289404
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-07-27 22:33:20 +0600

    working with deshboard

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/home/Home';
 import PageNotFound from './components/pageNotFound/PageNotFound';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
-import Login from './components/login/Login';
-import Registration from './components/registration/Registration';
 import RegistrationFull from './components/registration/RegistrationFull';
 import LoginFull from './components/login/LoginFull';
+import Dashboard from './components/dashboard/Dashboard';
+import Region from './components/dashboard/Region';
+import Area from './components/dashboard/Area';
 
 
 function App() {
@@ -21,6 +20,10 @@ function App() {
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/login' element={<LoginFull></LoginFull>}></Route>
         <Route path='/registration' element={<RegistrationFull></RegistrationFull>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}>
+          <Route index path='region' element={<Region></Region>}></Route>
+          <Route path ='area' element={<Area></Area>}></Route>
+        </Route>
       </Routes>
 
     </div>
