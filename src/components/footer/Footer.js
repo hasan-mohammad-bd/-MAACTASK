8672fd852be0a5fd91359d3cfbb5658ee23f3dce
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-07-27 12:18:38 +0600

    adding footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import logo from '../../Image/logo/FieldX (1).png';
+import './Footer.css';
+import soc1 from '../../Image/social/013-twitter.png';
+import soc2 from '../../Image/social/010-linkedin.png';
+import soc3 from '../../Image/social/001-facebook.png';
 
 const Footer = () => {
     return (
-        <div>
-            This is footer section.
+        <div className='footer'>
+            <div className='footer-text-img'>
+            <img src={logo} alt="" />
+            <p className='footer-doc'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem <br /> accusantium doloremque laudantium, totam rem aperiam eaque.</p>
+            <div className="social">
+                <img className='soc' src={soc1} alt="" />
+                <img className='soc' src={soc2} alt="" />
+                <img className='soc' src={soc3} alt="" />
+            </div>
+            </div>
+            <div className='lower'>
+            <div className='hr'></div>
+            <p className="copyright">&copy; All rights reserve by <span>MAAC</span></p>
+            </div>
         </div>
     );
 };
